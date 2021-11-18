89c558e961b39f96760d559b9ad96ffe48b6bbc3
Oscar Hederlig <oscar.hederlig@gmail.com>
2021-11-18 19:35:09 +0100

    Got 2 boxes only fixed. Color result is next

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ let checkAwnser = document.querySelector("#getResults");
 let list = (document.querySelector("#list").style.listStyle = "none");
 let awnser = document.querySelectorAll("[value='true']");
 let results = document.querySelector("#results");
-
+//Från länken
+var checks = document.querySelectorAll(".towns");
 
 button.addEventListener("click", () => {
   let body = document.querySelector("body");
@@ -15,6 +16,17 @@ button.addEventListener("click", () => {
     body.style.color = "red";
   }
 });
+
+// https://stackoverflow.com/questions/43456868/javascript-limit-selected-checkboxes-to-2
+// Studera och lär dig.
+var max = 2;
+for (var i = 0; i < checks.length; i++)
+  checks[i].onclick = selectiveCheck;
+function selectiveCheck (event) {
+  var checkedChecks = document.querySelectorAll(".towns:checked");
+  if (checkedChecks.length >= max + 1)
+    return false;
+}
 
 checkAwnser.addEventListener("click", () => {
   let counter = 0;
