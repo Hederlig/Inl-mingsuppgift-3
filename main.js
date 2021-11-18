01961fae1b74bd06d3788b4bd19d4d87c586983f
Oscar Hederlig <oscar.hederlig@gmail.com>
2021-11-18 18:44:17 +0100

    counter is complett

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 let button = document.querySelector("#changeButton");
-let list = document.querySelector("#list").style.listStyle = "none"
+let checkAwnser = document.querySelector("#getResults");
+let list = (document.querySelector("#list").style.listStyle = "none");
+let awnser = document.querySelectorAll("[value='true']");
+let results = document.querySelector("#results");
+
 
 button.addEventListener("click", () => {
   let body = document.querySelector("body");
-    if (body.style.background === "black") {
+  if (body.style.background === "black") {
     body.style.background = "white";
-    body.style.color = "black"
+    body.style.color = "black";
   } else {
     body.style.background = "black";
-    body.style.color = "red"
+    body.style.color = "red";
   }
 });
+
+checkAwnser.addEventListener("click", () => {
+  let counter = 0;
+  awnser.forEach((input) => {
+    if (input.checked) {
+      counter++;
+    }
+  });
+  results.innerHTML = " Your final score is " + counter + " out of 7";
+});
+
